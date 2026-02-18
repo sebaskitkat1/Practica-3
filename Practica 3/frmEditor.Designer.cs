be77d998eeaa00eea899d60082ebf33d94c316a1
fcoan <gonzalezbarbosasebastian@gmail.com>
2026-02-18 16:13:48 -0600

    Se agrega una funcion para guardar caracteristicas del richTextBox en un archivo XML que se guarda en la misma carpeta que el txt

---
+++ Practica 3/frmEditor.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Calculadora.Formularios
+{
+    partial class frmEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mnsEditor = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            guardarComoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            editarToolStripMenuItem = new ToolStripMenuItem();
+            fuenteToolStripMenuItem = new ToolStripMenuItem();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            stsEditor = new StatusStrip();
+            tssStatus = new ToolStripStatusLabel();
+            rtbEditor = new RichTextBox();
+            opfEditor = new OpenFileDialog();
+            sfdEditor = new SaveFileDialog();
+            ftdEditor = new FontDialog();
+            cldEditor = new ColorDialog();
+            colorDeFondoToolStripMenuItem = new ToolStripMenuItem();
+            mnsEditor.SuspendLayout();
+            stsEditor.SuspendLayout();
+            SuspendLayout();
+            // 
+            // mnsEditor
+            // 
+            mnsEditor.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, editarToolStripMenuItem });
+            mnsEditor.Location = new Point(0, 0);
+            mnsEditor.Name = "mnsEditor";
+            mnsEditor.Size = new Size(800, 24);
+            mnsEditor.TabIndex = 0;
+            mnsEditor.Text = "menuStrip1";
+            mnsEditor.ItemClicked += mnsEditor_ItemClicked;
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, abrirToolStripMenuItem, guardarToolStripMenuItem, guardarComoToolStripMenuItem, salirToolStripMenuItem, toolStripSeparator1 });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(150, 22);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            nuevoToolStripMenuItem.Click += nuevoToolStripMenuItem_Click;
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(150, 22);
+            abrirToolStripMenuItem.Text = "Abrir";
+            abrirToolStripMenuItem.Click += abrirToolStripMenuItem_Click;
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(150, 22);
+            guardarToolStripMenuItem.Text = "Guardar";
+            guardarToolStripMenuItem.Click += guardarToolStripMenuItem_Click;
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            guardarComoToolStripMenuItem.Size = new Size(150, 22);
+            guardarComoToolStripMenuItem.Text = "Guardar como";
+            guardarComoToolStripMenuItem.Click += guardarComoToolStripMenuItem_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(150, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(147, 6);
+            // 
+            // editarToolStripMenuItem
+            // 
+            editarToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { fuenteToolStripMenuItem, colorToolStripMenuItem, colorDeFondoToolStripMenuItem });
+            editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            editarToolStripMenuItem.Size = new Size(49, 20);
+            editarToolStripMenuItem.Text = "Editar";
+            // 
+            // fuenteToolStripMenuItem
+            // 
+            fuenteToolStripMenuItem.Name = "fuenteToolStripMenuItem";
+            fuenteToolStripMenuItem.Size = new Size(180, 22);
+            fuenteToolStripMenuItem.Text = "Fuente";
+            fuenteToolStripMenuItem.Click += fuenteToolStripMenuItem_Click;
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(180, 22);
+            colorToolStripMenuItem.Text = "Color";
+            colorToolStripMenuItem.Click += colorToolStripMenuItem_Click;
+            // 
+            // stsEditor
+            // 
+            stsEditor.Items.AddRange(new ToolStripItem[] { tssStatus });
+            stsEditor.Location = new Point(0, 428);
+            stsEditor.Name = "stsEditor";
+            stsEditor.Size = new Size(800, 22);
+            stsEditor.TabIndex = 1;
+            stsEditor.Text = "statusStrip1";
+            stsEditor.ItemClicked += stsEditor_ItemClicked;
+            // 
+            // tssStatus
+            // 
+            tssStatus.Name = "tssStatus";
+            tssStatus.Size = new Size(60, 17);
+            tssStatus.Text = "0 Palabras";
+            tssStatus.Click += tssStatus_Click;
+            // 
+            // rtbEditor
+            // 
+            rtbEditor.Dock = DockStyle.Fill;
+            rtbEditor.Location = new Point(0, 24);
+            rtbEditor.Name = "rtbEditor";
+            rtbEditor.Size = new Size(800, 404);
+            rtbEditor.TabIndex = 2;
+            rtbEditor.Text = "";
+            rtbEditor.TextChanged += rtbEditor_TextChanged;
+            // 
+            // opfEditor
+            // 
+            opfEditor.Filter = "\"Archivos de texto\"|*.txt";
+            // 
+            // sfdEditor
+            // 
+            sfdEditor.Filter = "\"Archivos de texto\"|*.txt";
+            // 
+            // ftdEditor
+            // 
+            ftdEditor.Apply += fontDialog1_Apply;
+            // 
+            // colorDeFondoToolStripMenuItem
+            // 
+            colorDeFondoToolStripMenuItem.Name = "colorDeFondoToolStripMenuItem";
+            colorDeFondoToolStripMenuItem.Size = new Size(180, 22);
+            colorDeFondoToolStripMenuItem.Text = "Color de fondo";
+            colorDeFondoToolStripMenuItem.Click += colorDeFondoToolStripMenuItem_Click;
+            // 
+            // frmEditor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(rtbEditor);
+            Controls.Add(stsEditor);
+            Controls.Add(mnsEditor);
+            MainMenuStrip = mnsEditor;
+            Name = "frmEditor";
+            Text = "Editor de Texto";
+            mnsEditor.ResumeLayout(false);
+            mnsEditor.PerformLayout();
+            stsEditor.ResumeLayout(false);
+            stsEditor.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip mnsEditor;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private StatusStrip stsEditor;
+        private RichTextBox rtbEditor;
+        private ToolStripMenuItem guardarComoToolStripMenuItem;
+        private OpenFileDialog opfEditor;
+        private SaveFileDialog sfdEditor;
+        private ToolStripStatusLabel tssStatus;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem fuenteToolStripMenuItem;
+        private FontDialog ftdEditor;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ColorDialog cldEditor;
+        private ToolStripMenuItem colorDeFondoToolStripMenuItem;
+    }
+}
---
+++ Practica 3/frmEditor.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Calculadora.Formularios
+{
+    public partial class frmEditor : Form
+    {
+        Boolean saved = false;
+        string path = "";
+        int contadorPalabras = 0;
+        string texto = "";
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void rtbEditor_TextChanged(object sender, EventArgs e)
+        {
+            texto = rtbEditor.Text;
+            string[] palabras = texto.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            contadorPalabras = palabras.Length;
+            this.tssStatus.Text = $"Numero de palabras: {contadorPalabras}";
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.opfEditor.ShowDialog();
+            if (File.Exists(opfEditor.FileName))
+            {
+                rtbEditor.Text = File.ReadAllText(opfEditor.FileName);
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!saved)
+            {
+                guardar();
+                saved = true;
+            }
+            else
+            {
+                using (StreamWriter archivo = new StreamWriter(path))
+                {
+                    archivo.Write(rtbEditor.Text);
+                    guardarXml();
+                }
+            }
+        }
+
+        private void guardarXml()
+        {
+            string carpeta = Path.GetDirectoryName(path);
+            string rutaXml = Path.Combine(carpeta, "Caracteristicas.xml");
+            XElement xml = new XElement("Caracteristicas");
+            xml.Add(new XElement("Caracteristicas"),
+            new XElement("Fuente", rtbEditor.Font.ToString()),
+            new XElement("Color", rtbEditor.ForeColor.ToArgb().ToString()),
+            new XElement("ColorDeFondo", rtbEditor.BackColor.ToArgb().ToString())
+        );
+            try
+            {
+                xml.Save(rutaXml);
+                MessageBox.Show("Archivo guardado correctamente.", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void guardar()
+        {
+            if (this.sfdEditor.ShowDialog() == DialogResult.OK)
+            {
+                path = sfdEditor.FileName;
+                using (StreamWriter archivo = new StreamWriter(path))
+                {
+                    archivo.Write(rtbEditor.Text);
+                }
+                guardarXml();
+            }
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            guardar();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtbEditor.Clear();
+            path = "";
+            saved = false;
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void stsEditor_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void tssStatus_Click(object sender, EventArgs e)
+        {
+            string[] palabras = texto.Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] parrafos = texto.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            MessageBox.Show("Estadisicas: \n\nPalabras: " + contadorPalabras + "\nCaracteres(con espacio): " + texto.Length.ToString()
+                + "\nParrafos: " + parrafos.Length.ToString(),
+                "Contador de palabras", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void fuenteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ftdEditor.ShowDialog() == DialogResult.OK)
+            {
+                rtbEditor.Font = ftdEditor.Font;
+            }
+        }
+
+        private void mnsEditor_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cldEditor.ShowDialog() == DialogResult.OK)
+            {
+                rtbEditor.ForeColor = cldEditor.Color;
+            }
+        }
+
+        private void colorDeFondoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cldEditor.ShowDialog() == DialogResult.OK)
+            {
+                rtbEditor.BackColor = cldEditor.Color;
+            }
+        }
+    }
+}
